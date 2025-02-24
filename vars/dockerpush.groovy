d0086030e27953390bfcf7940526b68d8f17d13f
Swayam Pradeep Nakshane <nakshaneswayamatha24@gmail.com>
2025-02-24 20:56:25 +0530

    Update dockerpush.groovy

--- vars/dockerpush.groovy
+++ vars/dockerpush.groovy
@@ -1,3 +1,12 @@
-def call(){
-  
+def call(string credID,string imagename){
+   withCredentials([usernamePassword(
+                    credentialsId:"$(credID)",
+                    passwordVariable: "dockerHubPass",
+                    usernameVariable: "dockerHubUser"
+                )]){
+                
+                sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
+                sh "docker tag "$(imagename)" ${env.dockerHubUser}/$(imagename)"
+                sh "docker push ${env.dockerHubUser}/$(imagename):latest"
+                }
 }
